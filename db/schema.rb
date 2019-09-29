d94e5d92dae08c9a071225f35071d3cc03576ec3
chiroru <chi.r.r.333@gmail.com>
2019-09-30 00:14:40 +0900

    Generate tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_01_043645) do
+ActiveRecord::Schema.define(version: 2019_09_26_085346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.boolean "finished"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "nickname"
