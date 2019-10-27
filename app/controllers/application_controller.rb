216ec9effa343774a27b2a9e2e199d8362b49bbe
chiroru <chi.r.r.333@gmail.com>
2019-10-27 11:23:54 +0900

    current_userに関するヘルパーメソッドの定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  private
+
+  def current_user
+    return unless session[:user_id]
+
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in?
+    session[:user_id].present?
+  end
+
+  def authenticate
+    return if logged_in?
+
+    redirect_to root_path, alert: 'ログインしてください'
+  end
 end
