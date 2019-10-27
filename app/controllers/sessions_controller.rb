568a756fd58f35b7c9d848e8fd43ed395e8f62cf
chiroru <chi.r.r.333@gmail.com>
2019-10-27 11:22:16 +0900

    Correct sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
   def create
     session[:user_id] = authenticate_user(request.env['omniauth.auth']).id
-    flash[:notice] = "ユーザ認証が完了しました。"
+    flash[:notice] = 'ユーザ認証が完了しました。'
     redirect_to root_path
   end
 
   def destroy
     reset_session
-    flash[:notice] = "ログアウトしました。"
+    flash[:notice] = 'ログアウトしました。'
     redirect_to root_path
   end
 
